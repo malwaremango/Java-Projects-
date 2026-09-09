835e04a57506f6269937e626ab8ec1dd979e9208
Usman Ahmad <91467380+malwaremango@users.noreply.github.com>
2026-09-09 14:03:11 +0200

    Update passwordchecker.java

--- passwordchecker.java
+++ passwordchecker.java
@@ -1,22 +1,28 @@
+import java.util.Scanner;
+
 public class EnkelSikkerhet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String passord;
+        boolean erSterkt = false;
 
-        System.out.print("Skriv inn et passord: ");
-        String passord = scanner.nextLine();
+        while (!erSterkt) {
+            System.out.print("Skriv inn et passord: ");
+            passord = scanner.nextLine();
 
-        while 
-        if (passord.length() < 8) {
-            System.out.println("For kort! Bruk minst 8 tegn.");
-        } else if (!passord.matches(".*[0-9].*")) {
-            System.out.println("Mangler tall. Legg til et tall.");
-        } else if (!passord.matches(".*[A-Z].*")) {
-            System.out.println("Mangler stor bokstav.");
-        } else {
-            System.out.println("Passordet ser greit ut!");
+            if (passord.length() < 8) {
+                System.out.println("For kort! Bruk minst 8 tegn.\n");
+            } else if (!passord.matches(".*[0-9].*")) {
+                System.out.println("Mangler tall. Legg til et tall.\n");
+            } else if (!passord.matches(".*[A-Z].*")) {
+                System.out.println("Mangler stor bokstav.\n");
+            } else {
+                System.out.println("Passordet ser greit ut!");
+                erSterkt = true;
+            }
         }
-      
+
         scanner.close();
     }
 }
